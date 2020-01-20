42671d78f9fcbd9cb9c5ca231d94445290c7d192
Luciano Tamanaha <lucianotamanaha@gmail.com>
2020-01-20 07:51:46 -0300

    First commit - Practice 1

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <app-server-list></app-server-list>
+      <app-server-detail></app-server-detail>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import ServerList from './ServerList.vue';
+    import ServerDetail from './ServerDetail.vue';
+    import Footer from './Footer.vue'
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-server-list': ServerList,
+            'app-server-detail': ServerDetail,
+            'app-footer': Footer,
+        }
+    }
+</script>
+
+<style>
+</style>
