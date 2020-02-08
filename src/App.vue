83e213b4d99495742dce93f8be03213f545e7188
lucianotamanaha <lucianotamanaha@gmail.com>
2020-02-08 15:42:39 -0300

    Master default

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,7 @@
     </v-app-bar>
     <v-content>
       <v-container fluid>
-        <v-row>
-          <v-col>
-            <v-card>
-              <app-servers></app-servers>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card>
-              <app-server-details></app-server-details>
-            </v-card>
-          </v-col>
-        </v-row>
+        
       </v-container>
     </v-content>
     <v-footer>
@@ -28,8 +17,6 @@
 <script>
 import AppHeader from "./components/Shared/Header";
 import AppFooter from "./components/Shared/Footer";
-import AppServers from "./components/Server/Servers";
-import AppServerDetails from "./components/Server/ServerDetails";
 
 export default {
   name: "App",
@@ -37,8 +24,6 @@ export default {
   components: {
     AppHeader,
     AppFooter,
-    AppServers,
-    AppServerDetails
   },
 
   data: () => ({
