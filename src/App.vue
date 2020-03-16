b18aec2ea289e047cf41c1579080a697a080b279
lucianotamanaha <lucianotamanaha@gmail.com>
2020-03-15 21:39:32 -0300

    exercicio 4

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,58 @@
     </v-app-bar>
     <v-content>
       <v-container fluid>
-        <v-row>
-          <v-col>
-            <v-btn color="blue" class="mr-2" @click="selectComponent = 'Blue'">Load Blue Template</v-btn>
-            <v-btn color="green" class="mr-2" @click="selectComponent = 'Green'">Load Green Template</v-btn>
-            <v-btn color="red" @click="selectComponent = 'Red'">Load Red Template</v-btn>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <component :is="selectComponent">
-              <p>This is the Content</p>
-            </component>
-          </v-col>
-        </v-row>
+        <v-form v-if="!submitted">
+          <full-name v-model="fullName"></full-name>
+          <v-row>
+            <v-col>
+              <v-text-field label="email" filled v-model="email"></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field label="Password" type="password" filled v-model="password"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-radio-group v-model="storeData">
+                <v-radio label="Yes" value="Yes"></v-radio>
+                <v-radio label="No" value="No"></v-radio>
+              </v-radio-group>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-btn
+              type="submit"
+              @click.prevent="submitted=true"
+            >Submit the Form</v-btn>
+          </v-row>
+        </v-form>
+
+        <v-card>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Full Name:</v-list-item-title>
+              <v-list-item-subtitle>{{ fullName }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>E-mail</v-list-item-title>
+              <v-list-item-subtitle>{{ email }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Password</v-list-item-title>
+              <v-list-item-subtitle>{{ password }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Store in database?</v-list-item-title>
+              <v-list-item-subtitle>{{ storeData }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
       </v-container>
     </v-content>
     <v-footer>
@@ -28,11 +66,9 @@
 </template>
 
 <script>
-import AppHeader from "./components/Shared/Header"
-import AppFooter from "./components/Shared/Footer"
-import Blue from "./components/Blue"
-import Green from "./components/Green"
-import Red from "./components/Red"
+import AppHeader from "./components/Shared/Header";
+import AppFooter from "./components/Shared/Footer";
+import FullName from "./FullName";
 
 export default {
   name: "App",
@@ -40,15 +76,17 @@ export default {
   components: {
     AppHeader,
     AppFooter,
-    Blue,
-    Green,
-    Red,
+    FullName
   },
 
   data: () => {
     return {
-      selectComponent: 'Blue'
-    }
+      fullName: "Luciano Tamanaha",
+      email: "",
+      password: "",
+      storeData: "",
+      submitted: false,
+    };
   }
 };
 </script>
---
+++ src/FullName.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-text-field label="First Name" filled :value="firstName" @input="nameChanged(true, $event)"></v-text-field>
+    </v-col>
+    <v-col>
+      <v-text-field label="Last Name" filled :value="lastName" @input="nameChanged(false, $event)"></v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String
+  },
+  methods: {
+    nameChanged(isFirst, event) {
+      let name = '';
+      if (isFirst) {
+        name = event.target.value + ' ' + this.lastName;
+      } else {
+        name = this.firstName + ' ' + event.target.value;
+      }
+      this.value = name;
+      this.$emit('input', this.value);
+    }
+  },
+  computed: {
+    firstName() {
+      return this.value.split(" ")[0];
+    },
+    lastName() {
+      return this.value.split(" ")[1];
+    }
+  }
+};
+</script>
